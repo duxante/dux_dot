181c8ec2f18127f31cc9e38642c08f5704fc9e54
duxante <spomenko.dukic@gmail.com>
2023-12-19 19:21:07 +0100

    edit responsive

--- src/features/flexibileComponents/components/radioButtons.jsx
+++ src/features/flexibileComponents/components/radioButtons.jsx
@@ -3,16 +3,18 @@ import Radio from '@mui/joy/Radio';
 
 const RadioButtonLabel = () => {
   return (
-    <Radio
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        maxWidth: '400px',
-        width: '100%',
-      }}
-      label="Radio Button"
-      defaultChecked
-    />
+    <div className="radioButtonHolder">
+      <Radio
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          maxWidth: '400px',
+          width: '100%',
+        }}
+        label="Radio Button"
+        defaultChecked
+      />
+    </div>
   );
 };
 
